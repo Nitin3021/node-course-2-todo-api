868d66fe93f24c5a055b242c45a4580debe8a7a1
Nitin Pillai <nitinpillai30@yahoo.com>
2018-06-09 23:24:07 +0800

    Add/Del script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log(`Unable to connect MongoDB server, ${err}`);
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5b1b93d595a9f812fc494f1f')
+  }).then((result) => {
+    console.log(result);
+  })
+
+  // db.close();
+});
