5f45f4d7eb254931c05b31db554a9ce55c19d009
Nitin Pillai <nitinpillai30@yahoo.com>
2018-06-13 23:31:58 +0800

    Setup app for Heroku 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://Nitin:Heroku123@ds159020.mlab.com:59020/mongo-data' || 'mongodb://localhost:27017/TodoApp');
 
 module.export = {mongoose};
