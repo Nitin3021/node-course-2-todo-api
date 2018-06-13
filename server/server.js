1f644a3e113bd9aaf52690b5e778bf0f3bd3aac2
Nitin Pillai <nitinpillai30@yahoo.com>
2018-06-13 21:48:55 +0800

    Add GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -23,9 +25,26 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send(JSON.stringify(todos, undefined, 2));
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send({});
+  };
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({});
+    }
+    res.send(todo);
+  }).catch((e) => {
+    return res.status(404).send({});
   });
 });
 
