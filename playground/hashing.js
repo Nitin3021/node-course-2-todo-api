c7fecc012e21b1d0ec72b39b85fe47985352487f
Nitin Pillai <nitinpillai30@yahoo.com>
2018-06-16 16:50:05 +0800

    Hash User password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(11, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+var hashedPassword = '$2a$11$Z4oUO1CWZ0zOImZhwLZoK.C96L1urpY9mPLWYyC055N0QrjrNrM6.';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 // var message = 'I am user number 21';
 // var hash = SHA256(message).toString();
 //
